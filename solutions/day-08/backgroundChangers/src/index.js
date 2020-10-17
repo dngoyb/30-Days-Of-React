11c7b4c32f7488ee0a4b987fed3f2f5c51a44f92
Ngoy banza <dngoyb@gmail.com>
2020-10-17 17:12:21 +0200

    visit country select

--- solutions/day-08/backgroundChangers/src/index.js
+++ solutions/day-08/backgroundChangers/src/index.js
@@ -2,295 +2,404 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import asabenehImage from './images/asabeneh.jpg';
+import { countriesData } from './data/countries.js';
 
 // Fuction to show month date year
 
 const showDate = (time) => {
-	const months = [
-		'January',
-		'February',
-		'March',
-		'April',
-		'May',
-		'June',
-		'July',
-		'August',
-		'September',
-		'October',
-		'November',
-		'December',
-	];
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ];
 
-	const month = months[time.getMonth()].slice(0, 3);
-	const year = time.getFullYear();
-	const date = time.getDate();
-	return ` ${month} ${date}, ${year}`;
+    const month = months[time.getMonth()].slice(0, 3);
+    const year = time.getFullYear();
+    const date = time.getDate();
+    return ` ${month} ${date}, ${year}`;
 };
 
 // User Card Component
 const UserCard = ({ user: { firstName, lastName, image } }) => (
-	<div className='user-card'>
-		<img src={image} alt={firstName} />
-		<h2>
-			{firstName}
-			{lastName}
-		</h2>
-	</div>
+    <div className='user-card'>
+        <img src={image} alt={firstName} />
+        <h2>
+            {firstName}
+            {lastName}
+        </h2>
+    </div>
 );
 
 // A button component
 const Button = ({ text, onClick, style }) => (
-	<button style={style} onClick={onClick}>
-		{text}
-	</button>
+    <button style={style} onClick={onClick}>
+        {text}
+    </button>
 );
 
 // CSS styles in JavaScript Object
 const buttonStyles = {
-	backgroundColor: '#61dbfb',
-	padding: 10,
-	border: 'none',
-	borderRadius: 5,
-	margin: 3,
-	cursor: 'pointer',
-	fontSize: 18,
-	color: 'white',
+    backgroundColor: '#61dbfb',
+    padding: 10,
+    border: 'none',
+    borderRadius: 5,
+    margin: 3,
+    cursor: 'pointer',
+    fontSize: 18,
+    color: 'white',
 };
 
-let headerStyles = {
-	backgroundColor: '#61dbfb',
-};
+let colors = {
+    headerStyles: {
+        backgroundColor: '#61dbfb',
+    },
 
-let footerStyles = {
-	backgroundColor: '#6cf',
-};
-let hrStyles = {
-	opacity: 0,
+    footerStyles: {
+        backgroundColor: '#6cf',
+    },
+    hrStyle: {
+        opacity: 0,
+    },
 };
 // class based component
 class Header extends React.Component {
-	constructor(props) {
-		super(props);
-		// the code inside the constructor run before any other code
-	}
-	render() {
-		console.log(this.props.data);
-		const {
-			welcome,
-			title,
-			subtitle,
-			author: { firstName, lastName },
-			date,
-		} = this.props.data;
+    constructor(props) {
+        super(props);
+        // the code inside the constructor run before any other code
+    }
+    render() {
+        const {
+            welcome,
+            title,
+            subtitle,
+            author: { firstName, lastName },
+            date,
+        } = this.props.data;
 
-		return (
-			<header style={this.props.styles} style={headerStyles}>
-				<div className='header-wrapper'>
-					<h1>{welcome}</h1>
-					<h2>{title}</h2>
-					<h3>{subtitle}</h3>
-					<p>
-						{firstName} {lastName}
-					</p>
-					<small>{date}</small>
-				</div>
-			</header>
-		);
-	}
+        return (
+            <header style={this.props.styles} style={colors.headerStyles}>
+                <div className='header-wrapper'>
+                    <h1>{welcome}</h1>
+                    <h2>{title}</h2>
+                    <h3>{subtitle}</h3>
+                    <p>
+                        {firstName} {lastName}
+                    </p>
+                    <small>{date}</small>
+                </div>
+            </header>
+        );
+    }
 }
 
 const Count = ({ count, addOne, minusOne }) => (
-	<div>
-		<h1>{count} </h1>
-		<div>
-			<Button text='+1' onClick={addOne} style={buttonStyles} />
-			<Button text='-1' onClick={minusOne} style={buttonStyles} />
-		</div>
-	</div>
+    <div>
+        <h1>{count} </h1>
+        <div>
+            <Button text='+1' onClick={addOne} style={buttonStyles} />
+            <Button text='-1' onClick={minusOne} style={buttonStyles} />
+        </div>
+    </div>
 );
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 
 // TechList Component
 // class base component
 class TechList extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		const { techs } = this.props;
-		const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>);
-		return techsFormatted;
-	}
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const { techs } = this.props;
+        const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>);
+        return techsFormatted;
+    }
 }
 
 // Main Component
 // Class Component
 class Main extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		const {
-			techs,
-			user,
-			greetPeople,
-			handleTime,
-			changeBackground,
-			count,
-			addOne,
-			minusOne,
-		} = this.props;
-		return (
-			<main>
-				<div className='main-wrapper'>
-					<p>Prerequisite to get started react.js:</p>
-					<ul>
-						<TechList techs={techs} />
-					</ul>
-					<UserCard user={user} />
-					<Button
-						text='Greet People'
-						onClick={greetPeople}
-						style={buttonStyles}
-					/>
-					<Button text='Show Time' onClick={handleTime} style={buttonStyles} />
-					<Button
-						text='Change Background'
-						onClick={changeBackground}
-						style={buttonStyles}
-					/>
-					<Count count={count} addOne={addOne} minusOne={minusOne} />
-				</div>
-			</main>
-		);
-	}
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const {
+            techs,
+            user,
+            greetPeople,
+            handleTime,
+            changeBackground,
+            count,
+            addOne,
+            minusOne,
+        } = this.props;
+        return (
+            <main>
+                <div className='main-wrapper'>
+                    <p>Prerequisite to get started react.js:</p>
+                    <ul>
+                        <TechList techs={techs} />
+                    </ul>
+                    <UserCard user={user} />
+                    <Button
+                        text='Greet People'
+                        onClick={greetPeople}
+                        style={buttonStyles}
+                    />
+                    <Button
+                        text='Show Time'
+                        onClick={handleTime}
+                        style={buttonStyles}
+                    />
+                    <Button
+                        text='Change Background'
+                        onClick={changeBackground}
+                        style={buttonStyles}
+                    />
+                    <Count count={count} addOne={addOne} minusOne={minusOne} />
+                </div>
+            </main>
+        );
+    }
 }
 
 // Footer Component
 // Class component
 class Footer extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<footer style={footerStyles}>
-				<div className='footer-wrapper'>
-					<p>Copyright {this.props.date.getFullYear()}</p>
-				</div>
-			</footer>
-		);
-	}
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <footer style={colors.footerStyles}>
+                <div className='footer-wrapper'>
+                    <p>Copyright {this.props.date.getFullYear()}</p>
+                </div>
+            </footer>
+        );
+    }
+}
+
+const myStyles = {
+    container: {
+        width: '511px',
+        height: '428px',
+        margin: '2rem auto',
+        boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+    },
+    image: {
+        width: '350px',
+        height: '200px',
+        margin: '1rem auto',
+        boxShadow: '0 4px 5px 0 rgba(0,0,0,0.2)',
+    },
+    imgDiv: {
+        textAlign: 'center',
+    },
+};
+
+class Country extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const { selectCountry } = this.props;
+        const list = countriesData.map(
+            ({ flag, name, capital, languages, population, currency }) => {
+                let lang = languages.map((lan) => `${lan}`);
+                console.log(lang.join(',').replace(/,/g, ',    '));
+                return (
+                    <div>
+                        <div style={myStyles.container}>
+                            <div style={myStyles.imgDiv}>
+                                <img
+                                    src={flag}
+                                    alt='flag'
+                                    style={myStyles.image}
+                                />
+                            </div>
+                            <h2 style={{ textAlign: 'center' }}>{name}</h2>
+                            <div className='card-text'>
+                                <h5>
+                                    Capital: <span>{capital}</span>
+                                </h5>
+                                <h5>
+                                    Languages:{' '}
+                                    <span>
+                                        {languages
+                                            .map((lang) => `${lang}`)
+                                            .join(',')
+                                            .replace(/,/g, ', ')}
+                                    </span>
+                                </h5>
+                                <h5>
+                                    Population:
+                                    <span>
+                                        {' '}
+                                        {new Intl.NumberFormat().format(
+                                            population
+                                        )}
+                                    </span>
+                                </h5>
+                                <h5>
+                                    Currency:<span> {currency}</span>
+                                </h5>
+                            </div>
+                        </div>
+                        <div
+                            style={{
+                                margin: '0 auto 5rem auto',
+                                textAlign: 'center',
+                            }}>
+                            <Button
+                                text='Select Country'
+                                onClick={selectCountry}
+                                style={buttonStyles}
+                            />
+                        </div>
+                    </div>
+                );
+            }
+        );
+
+        return list[Math.floor(Math.random() * list.length)];
+    }
 }
 
 class App extends React.Component {
-	state = {
-		count: 0,
-		styles: {
-			backgroundColor: '',
-			color: '',
-		},
-	};
-	showDate = (time) => {
-		const months = [
-			'January',
-			'February',
-			'March',
-			'April',
-			'May',
-			'June',
-			'July',
-			'August',
-			'September',
-			'October',
-			'November',
-			'December',
-		];
+    state = {
+        count: 0,
+        styles: {
+            backgroundColor: '',
+            color: '',
+        },
+    };
+    showDate = (time) => {
+        const months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ];
 
-		const month = months[time.getMonth()].slice(0, 3);
-		const year = time.getFullYear();
-		const date = time.getDate();
-		return ` ${month} ${date}, ${year}`;
-	};
-	addOne = () => {
-		if (this.state.count === 'Zero') {
-			this.setState({ count: 1 });
-		} else {
-			this.setState({ count: this.state.count + 1 });
-		}
-	};
+        const month = months[time.getMonth()].slice(0, 3);
+        const year = time.getFullYear();
+        const date = time.getDate();
+        return ` ${month} ${date}, ${year}`;
+    };
+    addOne = () => {
+        if (this.state.count === 'Zero') {
+            this.setState({ count: 1 });
+        } else {
+            this.setState({ count: this.state.count + 1 });
+        }
+    };
 
-	// method which subtract one to the state
-	minusOne = () => {
-		if (this.state.count === 0) {
-			this.setState({ count: 'Zero' });
-		} else {
-			this.setState({ count: this.state.count - 1 });
-		}
-	};
-	handleTime = () => {
-		alert(this.showDate(new Date()));
-	};
-	greetPeople = () => {
-		alert('Welcome to 30 Days Of React Challenge, 2020');
-	};
-	changeBackground = () => {
-		const style = this.state.styles;
-		if (style.backgroundColor === '') {
-			headerStyles = { backgroundColor: '' };
-			footerStyles = { backgroundColor: '' };
-			hrStyles = {
-				opacity: 1,
-			};
-			this.setState({
-				styles: { backgroundColor: '#000', color: '#fff' },
-			});
-		} else {
-			this.setState({ styles: { backgroundColor: '' } });
-			headerStyles = { backgroundColor: '#61dbfb' };
-			footerStyles = { backgroundColor: '#6cf' };
-			hrStyles = {
-				opacity: 0,
-			};
-		}
-	};
-	render() {
-		const data = {
-			welcome: 'Welcome to 30 Days Of React',
-			title: 'Getting Started React',
-			subtitle: 'JavaScript Library',
-			author: {
-				firstName: 'Asabeneh',
-				lastName: 'Yetayeh',
-			},
-			date: 'Oct 7, 2020',
-		};
-		const techs = ['HTML', 'CSS', 'JavaScript'];
-		const date = new Date();
-		// copying the author from data object to user variable using spread operator
-		const user = { ...data.author, image: asabenehImage };
+    // method which subtract one to the state
+    minusOne = () => {
+        if (this.state.count === 0) {
+            this.setState({ count: 'Zero' });
+        } else {
+            this.setState({ count: this.state.count - 1 });
+        }
+    };
+    handleTime = () => {
+        alert(this.showDate(new Date()));
+    };
+    greetPeople = () => {
+        alert('Welcome to 30 Days Of React Challenge, 2020');
+    };
+    changeBackground = () => {
+        const style = this.state.styles;
+        if (style.backgroundColor === '') {
+            colors = {
+                headerStyles: { backgroundColor: '' },
+                footerStyles: { backgroundColor: '' },
+                hrStyle: { opacity: 1 },
+            };
+            this.setState({
+                styles: { backgroundColor: '#000', color: '#fff' },
+            });
+        } else {
+            this.setState({ styles: { backgroundColor: '' } });
+            colors = {
+                headerStyles: { backgroundColor: '#61dbfb' },
+                footerStyles: { backgroundColor: '#6cf' },
+                hrStyle: { opacity: 0 },
+            };
+        }
+    };
 
-		return (
-			<div className='app' style={this.state.styles}>
-				{this.state.backgroundColor}
+    selectCountry = () => {
+        const style = this.state.styles;
+        console.log('hello');
+        if (style.backgroundColor === '') {
+            this.setState({
+                styles: { backgroundColor: '' },
+            });
+        }
+    };
+    render() {
+        const data = {
+            welcome: 'Welcome to 30 Days Of React',
+            title: 'Getting Started React',
+            subtitle: 'JavaScript Library',
+            author: {
+                firstName: 'Asabeneh',
+                lastName: 'Yetayeh',
+            },
+            date: 'Oct 7, 2020',
+        };
+        const techs = ['HTML', 'CSS', 'JavaScript'];
+        const date = new Date();
+        // copying the author from data object to user variable using spread operator
+        const user = { ...data.author, image: asabenehImage };
 
-				{this.props.headerStyles}
-				<Header data={data} />
-				<hr style={hrStyles} />
-				<Main
-					user={user}
-					techs={techs}
-					handleTime={this.handleTime}
-					greetPeople={this.greetPeople}
-					changeBackground={this.changeBackground}
-					addOne={this.addOne}
-					minusOne={this.minusOne}
-					count={this.state.count}
-				/>
-				<Footer date={new Date()} />
-			</div>
-		);
-	}
+        return (
+            <div className='app' style={this.state.styles}>
+                {this.state.backgroundColor}
+
+                {this.props.headerStyles}
+                <Header data={data} />
+                <hr style={colors.hrStyle} />
+                <Main
+                    user={user}
+                    techs={techs}
+                    handleTime={this.handleTime}
+                    greetPeople={this.greetPeople}
+                    changeBackground={this.changeBackground}
+                    addOne={this.addOne}
+                    minusOne={this.minusOne}
+                    count={this.state.count}
+                />
+
+                <Country selectCountry={this.selectCountry} />
+                <hr style={colors.hrStyle} />
+
+                <Footer date={new Date()} />
+            </div>
+        );
+    }
 }
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById('root'));
